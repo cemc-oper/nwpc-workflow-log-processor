1137ac0e368674f803416543626d1992e9476bfa
perillaroc <perillaroc@gmail.com>
2020-02-13 18:50:00 +0800

    :bookmark: bump to version 2.2.0
    first version as a separate project.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open(path.join(here, "README.md"), encoding="utf-8") as f:
 setup(
     name="nwpc-workflow-log-processor",
 
-    version="2.0.0",
+    version="2.2.0",
 
     description="A log processor for workflow systems in NWPC.",
     long_description=long_description,
